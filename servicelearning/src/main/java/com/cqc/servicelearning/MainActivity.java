c0ad8ef8258bd4f9f5b7639dd87034e61b1c6ec6
caoqingchao1 <caoqingchao1@xiaomi.com>
2022-03-14 10:23:15 +0800

    service test

--- servicelearning/src/main/java/com/cqc/servicelearning/MainActivity.java
+++ servicelearning/src/main/java/com/cqc/servicelearning/MainActivity.java
@@ -2,17 +2,43 @@ package com.cqc.servicelearning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Service;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private final String TAG = "MainActivity";
 
     private Button start;
     private Button stop;
+    private Button btnbind;
+    private Button btncancel;
+    private Button btnstatus;
+
+    //保持所启动的Service的IBinder对象,同时定义一个ServiceConnection对象
+    TestService2.MyBinder binder;
+    private ServiceConnection conn = new ServiceConnection() {
+
+        //Activity与Service断开连接时回调该方法
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            System.out.println("------Service DisConnected-------");
+        }
+
+        //Activity与Service连接成功时回调该方法
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            System.out.println("------Service Connected-------");
+            binder = (TestService2.MyBinder) service;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +46,52 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent();
-        intent.setAction("com.jay.example.service.TEST_SERVICE1");
-        intent.setPackage(getPackageName());
+        testStartService();
 
+        testBindService();
+    }
+
+    private void testBindService() {
+
+        btnbind = findViewById(R.id.button_bind);
+        btncancel =  findViewById(R.id.button_cance);
+        btnstatus  = findViewById(R.id.button_getstatus);
+        final Intent intent = new Intent();
+        intent.setAction("com.jay.example.service.TEST_SERVICE2");
+        btnbind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //绑定service
+                bindService(intent, conn, Service.BIND_AUTO_CREATE);
+            }
+        });
+
+        btncancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //解除service绑定
+                unbindService(conn);
+            }
+        });
+
+        btnstatus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Service的count的值为:"
+                        + binder.getCount(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+
+    private void testStartService() {
         start = findViewById(R.id.button_start);
         stop = findViewById(R.id.button_stop);
 
-
-
+        Intent intent = new Intent();
+        intent.setAction("com.jay.example.service.TEST_SERVICE1");
+        intent.setPackage(getPackageName());
 
         start.setOnClickListener(new View.OnClickListener() {
             @Override
